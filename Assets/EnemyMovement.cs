c67a0f450dc93be0621944c561d67072627f4d28
Ricardo Lázaro Zermeño <caffeine305gmail.com>
2017-11-03 21:35:55 -0600

    movimiento listo y seleccionsble
    movimiento ya no es PARA NADA parabólico XD
    
    Pero no importa, está pasando jjsto lo que quiero que pase

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+
+    public float velocidad;
+    public Transform target;
+    public int enemyType;
+    float initialPosY;
+    float fixTargetPosX;
+    int checkErrorX;
+    Vector2 actualTarget;
+
+    //Clave enemigo:
+    //terrestre = 0, aereo = 1
+
+
+    void Start () {
+        velocidad = 2.0f;
+        initialPosY = transform.position.y;
+        fixTargetPosX = target.position.x;
+        checkErrorX = (int)transform.position.x - (int)target.position.x;
+        actualTarget = target.position;
+        Debug.Log(actualTarget);
+    }
+
+    void moveAerial()
+    {
+        float step = velocidad * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, actualTarget, step);
+    }
+
+    void moveTerra()
+    {
+        float step = velocidad * Time.deltaTime;
+        Vector2 moveFloor = new Vector2(fixTargetPosX,initialPosY);
+        Debug.Log("Punto Auxiliar" + moveFloor);
+       
+        transform.position = Vector2.MoveTowards(transform.position, moveFloor, step);
+        checkErrorX = (int)transform.position.x - (int)target.position.x;
+        Debug.Log("Error: " + checkErrorX);        
+    }
+
+    void FixedUpdate()
+    {
+
+        if (enemyType == 0)
+        {
+            if (checkErrorX != 0)
+                moveTerra();
+
+            if (checkErrorX == 0)
+                moveAerial();
+        
+        }else
+            moveAerial();
+    }
+}
